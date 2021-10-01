2c5e68d750f49fcd9a7407b4d777ade23e8ff092
KennySohl <kenny.sohl@gmail.com>
2021-10-01 12:16:57 +0200

    add router-dom
    added router-dom & Home/playlistpage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,16 @@
+import React from 'react'
 import './App.css'
 import Navigation from './components/Navigation'
 import Player from './components/player'
+
+import {
+  BrowserRouter as Router,
+  Route,
+  Link
+} from 'react-router-dom'
+
+import HomePage from './pages/HomePage'
+import PlaylistPage from './pages/PlaylistPage'
 
 function App() {
 
@@ -9,6 +19,11 @@ function App() {
       <Navigation />
 
       <main>
+
+        <Router>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/PlaylistPage" exact component={PlaylistPage} />
+        </Router>
 
       </main>
 
