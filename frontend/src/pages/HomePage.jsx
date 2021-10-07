ba9d315da8c92bcf3cf1e892e82fdaf52bd478ef
lemuringen <lemuringen91113@gmail.com>
2021-10-07 13:47:44 +0200

    Basic searchfield. Basic corresponding result list responding to searches.

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function HomePage() {
+    const [input, setInput] = useState('')
+    const [songs, setSongs] = useState()
+
+    async function searchSong() {
+        let response = await fetch('https://yt-music-api.herokuapp.com/api/yt/songs/' + input)
+        let result = await response.json()
+        console.log(result.content)
+        setSongs(result.content)
+    }
+
+    function songClick(song) {
+        console.log(song.name)
+        //  setCurrentVideoId(song.videoId)
+    }
+
     return (
         <div>
             <h1>Home Page</h1>
+            <div>
+                <input
+                    type="text"
+                    placeholder="search songs"
+                    onChange={e => setInput(e.target.value)}
+                />
+                <button onClick={searchSong}>Search</button>
+                <hr/>
+                <div>
+                    {songs && songs.map(song => (
+                        <div className="search-list-item" onClick={() => songClick(song)} key={song.videoId}>
+                            <img src={song.thumbnails[0].url}/>
+                            <div className="search-list-item-text-container">
+                                <span className="search-list-item-song-row"> Song: {song.name}</span>
+                                <span className="search-list-item-artist-row"> Artist: {song.artist.name}</span>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
