ced6f4bdca338e228f816031deb1451d2448eb4b
lemuringen <lemuringen91113@gmail.com>
2021-10-08 17:01:06 +0200

    Play corresponding songs when selecting (clicking) on songs in search result list. Play/pause button controlls playstatus. Currenttime and duration of song is collected but not actively pushed to DOM.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,39 +1,41 @@
-import React from 'react'
+import React, {createContext, useState} from 'react'
+import { PlayerContext } from './contexts/PlayerContext'
+
 import './App.css'
 import './Navigation.css'
-import Navigation from './components/Navigation'
-import Player from './components/player'
+
 
 import {
-  BrowserRouter as Router,
-  Route,
-  Link
+    BrowserRouter as Router,
+    Route,
+    Link
 } from 'react-router-dom'
 
 import HomePage from './pages/HomePage'
 import PlaylistPage from './pages/PlaylistPage'
+import Navigation from './components/Navigation'
+import Player from './components/Player'
 
 function App() {
 
-  return (
-    <div className="App">
-      <Router>
-        <Navigation />
-        <Route path="/" exact component={HomePage} />
-        <Route path="/PlaylistPage" exact component={PlaylistPage} />
-      </Router>
 
-      <main>
+    return (
+        <div className="App">
+            <Router>
+                <Navigation/>
+                <Route path="/" exact component={HomePage}/>
+                <Route path="/PlaylistPage" exact component={PlaylistPage}/>
+            </Router>
 
-
-
-      </main>
+            <main>
 
 
-      <Player />
+            </main>
+            {/* update current song whenever the videoId changes */}
+            <Player />
 
-    </div >
-  )
+        </div>
+    )
 }
 
 export default App
